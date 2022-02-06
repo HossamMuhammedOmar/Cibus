2df74b71bbd4e7cdfb0a9d648acb85c2854e79a9
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-02-06 22:57:33 +0200

    finish section one

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const images = document.querySelectorAll(".section-one-icons img");
-
+let counter = 1;
 setInterval(() => {
+  counter++;
   const image = document.querySelector(".section-one .change");
   image.classList.remove("change");
-}, 4000);
+  if (counter > images.length) {
+    counter = 1;
+    images[0].classList.add("change");
+  } else {
+    image.nextElementSibling.classList.add("change");
+  }
+}, 2000);
+
+// 4s remove first image and appear the next image
+//
